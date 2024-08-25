93977fb49f02b7ee07c08d4775155519c1214cdc
BAZIL UR REHMAN <bazilurrehman08@gmail.com>
2024-08-25 12:10:43 +0500

    Add files via upload

---
+++ js/assignment1.js
@@ -0,0 +1,33 @@
+// assignment no 1 💕
+
+// question no 1
+alert("hello world")
+
+// question no 2
+alert("hello world \n Pakistan")
+
+// assignment no 2 😃
+
+// question no 1
+var username = "bazil khan"
+
+var message = "hello world"
+alert(message)
+
+alert("john doe")
+
+alert("15 years old")
+
+alert("certified mobile aplication development")
+
+alert("pizza \npizz \npiz \npi \np")
+
+
+alert("my email address is bazilurrehamn08@gmail.com")
+
+alert("a smart way to learn software engineering \n from SMIT Malir.")
+
+
+document.write(" <h1> hello world from Saylani </h1>")
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
